f64b9f83070c2fba991f4b27a076aac788f8eeeb
telostia <telostia@gmail.com>
2019-02-20 12:37:00 +1100

    updated to latest 1.1.1 xln version

--- guides/lunarium_fork_update.sh
+++ guides/lunarium_fork_update.sh
@@ -10,11 +10,11 @@ sudo rm lunarium*
 sudo rm /usr/local/bin/lunarium*
 
 #get wallet files
-sudo wget https://github.com/LunariumCoin/lunarium/releases/download/v1.1.0/lunarium-1.1.0-x86_64-linux-gnu.tar.gz
+sudo wget https://github.com/LunariumCoin/lunarium/releases/download/v1.1.1/lunarium-1.1.1-x86_64-linux-gnu.tar.gz
 #tar -xvf lunarium-linux.tar.gz
 #untar and strip unwanted directories to current folder
-sudo chmod +x lunarium-1.1.0-x86_64-linux-gnu.tar.gz
-sudo tar --strip-components=2 -zxf lunarium-1.1.0-x86_64-linux-gnu.tar.gz
+sudo chmod +x lunarium-1.1.1-x86_64-linux-gnu.tar.gz
+sudo tar --strip-components=2 -zxf lunarium-1.1.1-x86_64-linux-gnu.tar.gz
 sudo chmod +x lunarium*
 sudo mv lunarium-cli lunariumd /usr/local/bin/
 #clean up
